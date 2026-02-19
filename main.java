467bb19809f0831dc258ec67b54a88371299f1a5
Rohannaa2m12 <kusnadis404@gmail.com>
2026-02-19 10:10:24 +0100

    Update main.java

--- main.java
+++ main.java
@@ -46,3 +46,51 @@ enum FlipOutcome {
 }
 
 enum GameTier {
+    PEASANT(0, "Peasant", BigDecimal.valueOf(0.01), BigDecimal.valueOf(0.5)),
+    DEGEN(1, "Degen", BigDecimal.valueOf(0.5), BigDecimal.valueOf(2)),
+    WHALE(2, "Whale", BigDecimal.valueOf(2), BigDecimal.valueOf(10)),
+    SATOSHI(3, "Satoshi", BigDecimal.valueOf(10), BigDecimal.valueOf(100));
+
+    private final int index;
+    private final String name;
+    private final BigDecimal minEth;
+    private final BigDecimal maxEth;
+
+    GameTier(int index, String name, BigDecimal minEth, BigDecimal maxEth) {
+        this.index = index;
+        this.name = name;
+        this.minEth = minEth;
+        this.maxEth = maxEth;
+    }
+
+    public int getIndex() { return index; }
+    public String getName() { return name; }
+    public BigDecimal getMinEth() { return minEth; }
+    public BigDecimal getMaxEth() { return maxEth; }
+
+    public static GameTier forWager(BigDecimal eth) {
+        for (GameTier t : values()) {
+            if (eth.compareTo(t.minEth) >= 0 && eth.compareTo(t.maxEth) < 0) return t;
+        }
+        return SATOSHI;
+    }
+}
+
+enum StreakType {
+    NONE(0),
+    WIN_STREAK(1),
+    LOSS_STREAK(2);
+
+    private final int id;
+
+    StreakType(int id) { this.id = id; }
+    public int getId() { return id; }
+}
+
+// ==================== Constants ====================
+
+final class BFIConstants {
+    static final int HOUSE_EDGE_BPS = 250;
+    static final int BPS_DENOM = 10000;
+    static final int WIN_MULTIPLIER_BPS = 9750;
+    static final BigDecimal MIN_BET_ETH = new BigDecimal("0.01");
